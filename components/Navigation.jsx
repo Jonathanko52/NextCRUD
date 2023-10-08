ad3929c1d56646cf2a1f12c57f02d54f24b67848
Jonk <jonathan.ko523@gmail.com>
2023-10-08 16:36:00 -0700

    Navigation bar edditing

--- components/Navigation.jsx
+++ components/Navigation.jsx
@@ -5,10 +5,23 @@ Renders a navigation component with a sticky header, containing a logo and a lin
 */
 
 import Link from 'next/link'
-import { Container } from '.'
+import { Container } from './Container'
 import Image from 'next/image'
-import { TbArrowBigRightFilled } from 'react-icons/tb'
+// import { TbArrowBigRightFilled } from 'react-icons/tb'
 
 export const Navigation = () => {
-  return <nav>navigation component</nav>
+ return( <div className="sticky top-0 backdrop-blur-xl bg-[rgba(0,0,0,0.8)] border-b border-slate-800 z-50">
+    <div className="flex justify-between py-5">
+      <Link href="/">
+        <Image src="/logo.png" alt="Family Guy" width={70} height={50} />
+      </Link>
+      <Link
+        href="/quiz"
+        className="flex items-center justify-center gap-1 px-5 font-semibold text-black transition-colors bg-green-500 rounded-md duration-600 hover:bg-green-600"
+      >
+        {/* <TbArrowBigRightFilled className="text-lg" /> */}
+        Take a Quiz
+      </Link>
+    </div>
+  </div>)
 }
