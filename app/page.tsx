2ce975d73611369eab14f1d75521ca0074077faf
Jonk <jonathan.ko523@gmail.com>
2023-10-08 17:35:01 -0700

    implementing apis

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,15 @@
 import Image from 'next/image'
+import { endpoint } from './../../utils/endpoint'
+
+async function getAllCharacters() {
+  const data = await fetch(`${endpoint}/characters`)
+
+  if (!data.ok) {
+    throw new Error('Failed to fetch data')
+  }
+
+  return data.json()
+}
 
 export default function Home() {
   return (
